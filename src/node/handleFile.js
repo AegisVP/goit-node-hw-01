660e2a91cd79c2d7aadf39758b7f6c42e93e4d12
AegisVP <vlad@pysarenko.com>
2022-10-25 17:07:18 +0200

    fix async

--- src/node/handleFile.js
+++ src/node/handleFile.js
@@ -1,30 +1,13 @@
-const fs = require("fs");
-const { log } = require("./log.js");
-const CONST = require("./constants.js");
+const fs = require('fs');
+const { log } = require('./log.js');
+const CONST = require('./constants.js');
 
 const readData = (filePath = CONST.defaultPath) => {
-	let fileBuffer = null;
-	try {
-		fileBuffer = fs.readFileSync(filePath, CONST.fileEncoding);
-		fileBuffer = JSON.parse(fileBuffer);
-	} catch (error) {
-		return error;
-	}
-
-	return fileBuffer;
+  return JSON.parse(fs.readFileSync(filePath, CONST.fileEncoding));
 };
 
-const writeData = (filePath = contactsPath, fileContents = "") => {
-	try {
-		return fs.writeFileSync(
-			filePath,
-			JSON.stringify(fileContents),
-			CONST.fileEncoding
-		);
-	} catch (error) {
-		return error;
-	}
+const writeData = (filePath = contactsPath, fileContents = '') => {
+  return fs.writeFileSync(filePath, JSON.stringify(fileContents), CONST.fileEncoding);
 };
 
 module.exports = { readData, writeData };
-// export { readData, writeData };
